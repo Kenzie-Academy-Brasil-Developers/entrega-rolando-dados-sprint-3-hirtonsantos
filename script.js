4e7dfea2a8320b55aaaf9b08fc80330268978ec6
Your Name <you@example.com>
2021-09-12 20:07:34 -0300

    enviando entrega

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
+let array = []
+function arrayInitialization(){
+    for (let i = 0; i <= 10; i++) {
+        array[i] = 0 
+    } return array
+}
+
+function lancamentos(){
+    arrayInitialization()
+    for (let i = 0; i < 1000; i++) {
+        let dadoOne = getRandomInt(1,7)
+        let dadoTwo = getRandomInt(1,7)
+        let soma = dadoOne + dadoTwo
+        array[soma - 2] += 1
+        } return (mostrarTexto(), graficoBarras())
+}
+document.write (lancamentos())
+
+function mostrarTexto(){
+    const body = document.querySelector("body")
+    const ul = document.createElement("ul")
+    for (let i = 0; i < array.length; i++) {
+        let li = document.createElement("li")
+        li.innerText = array[i]
+        ul.appendChild(li)
+    } 
+    body.appendChild(ul)
+}
+
+function graficoBarras() {
+    const body = document.querySelector("body")
+    for (let i = 0; i < array.length; i++) {
+        let div = document.createElement("div")
+        div.innerText = array[i]
+        div.style.marginTop = "auto"
+        div.style.height = array[i] + "px" 
+        div.style.width = `${array[i]}px`
+        body.appendChild(div)
+    } 
+} 
